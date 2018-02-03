0e9b330ebc3744079d7c0663ee297981eab10901
Ellen Lee <leechinghui.tw@gmail.com>
2018-02-03 23:56:58 +0800

    add index template and crud action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,38 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+
+    if params[:id]
+      @todo = Todo.find(params[:id])
+    else
+      @todo = Todo.new
+    end
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+
+    redirect_to todos_path
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(todo_params)
+
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title)
+  end
 end
