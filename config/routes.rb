7871e07e9429bc253cb807066943d45b998eed5e
Ellen Lee <leechinghui.tw@gmail.com>
2018-02-03 16:03:28 +0800

    set route for toggle_check

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "todos#index"
-  resources :todos
+  resources :todos do
+    member do
+      post :toggle_check
+    end
+  end
 end
